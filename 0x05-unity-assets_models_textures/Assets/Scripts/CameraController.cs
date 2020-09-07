9df0b4ae1d4c1251288eb217bf1e4af36c07b8dc
Beardocracy <travisjbearden@gmail.com>
2020-09-07 16:04:38 -0500

    Changed player movement to be relative to camera

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/CameraController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public Vector3 offset;
+    public float rotationSpeed = 5.0f;
+    
+    [Range(0.01f, 1.0f)]
+    public float smoothness = 0.5f;
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Quaternion camAngle = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * rotationSpeed, Vector3.up);
+        offset = camAngle * offset;
+
+        Vector3 newPos = player.transform.position + offset;
+
+        transform.position = Vector3.Slerp(transform.position, newPos, smoothness);
+
+        transform.LookAt(player.transform.position);
+    }
+}
--- 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
+++ 0x05-unity-assets_models_textures/Assets/Scripts/PlayerController.cs
@@ -6,29 +6,53 @@ public class PlayerController : MonoBehaviour
 {
     public Rigidbody rb;
     public float speed = 900f;
+    public float jumpHeight = 200f;
+    public Transform camTransform;
+    private int contact = 0;
     
     // Called once per frame.
     void FixedUpdate()
     {
+        
+        Vector3 movement = Vector3.zero;
         if (Input.GetKey("d"))
         {
-            rb.AddForce(speed * Time.deltaTime, 0, 0);
+            movement += camTransform.right;
+            movement *= speed * Time.deltaTime;
+            movement.y = 0f;
         }      
         if (Input.GetKey("a"))
         {
-            rb.AddForce(-speed * Time.deltaTime, 0, 0);
+            movement += -camTransform.right;
+            movement *= speed * Time.deltaTime;
+            movement.y = 0f;
         }
         if (Input.GetKey("w"))
         {
-            rb.AddForce(0, 0, speed * Time.deltaTime);
+            movement += camTransform.forward;
+            movement *= speed * Time.deltaTime;
+            movement.y = 0f;
         }
         if (Input.GetKey("s"))
         {
-            rb.AddForce(0, 0, -speed * Time.deltaTime);
+            movement += -camTransform.forward;
+            movement *= speed * Time.deltaTime;
+            movement.y = 0f;
         }
-        if(Input.GetKey("space"))
+        if(Input.GetKey("space") && contact > 0)
         {
-            rb.AddForce(0, speed * Time.deltaTime, 0);
+            movement.y = jumpHeight;
         }
+        rb.AddForce(movement);
+    }
+
+    // Checks if player is in contact with something.
+    private void OnCollisionEnter(Collision other)
+    {
+        contact += 1;
+    }
+    private void OnCollisionExit(Collision other)
+    {
+        contact -= 1;
     }
 }
