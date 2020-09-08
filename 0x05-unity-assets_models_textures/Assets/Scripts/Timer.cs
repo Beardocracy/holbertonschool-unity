3e233f4b882cf3fa9d6d78bd926f9888c3d606ba
Beardocracy <travisjbearden@gmail.com>
2020-09-08 15:55:34 -0500

    Added part 8

---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    private float timer = 0f;
+    public Text timerText;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        string min = String.Format("{0:0}", timer / 60);
+        string sec = String.Format("{0:00}", timer % 60);
+        string msec = String.Format("{0:.00}", timer % 1);
+        timerText.text = min + ":" + sec + msec;
+    }
+}
---
+++ 0x05-unity-assets_models_textures/Assets/Scripts/TimerTrigger.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerTrigger : MonoBehaviour
+{
+    public GameObject player;
+    private void OnCollisionExit(Collision other)
+    {
+        player.GetComponent<Timer>().enabled = true;
+    }
+}
