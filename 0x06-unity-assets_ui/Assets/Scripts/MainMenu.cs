9c9258b1690180168e9e261e3d37bf3553495d8e
Beardocracy <travisjbearden@gmail.com>
2020-09-23 14:10:12 -0500

    Added part 3

---
+++ 0x06-unity-assets_ui/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads a level.
+    /// </summary>
+    public void LevelSelect(int level)
+    {
+        Debug.Log("You want to load level " + level.ToString());
+        
+        switch (level)
+        {
+            case 1:
+                PlayerPrefs.SetString("prevScene", "Level01");
+                SceneManager.LoadScene("Level01");
+                break;
+            case 2:
+                PlayerPrefs.SetString("prevScene", "Level02");
+                SceneManager.LoadScene("Level02");
+                break;
+            case 3:
+                PlayerPrefs.SetString("prevScene", "Level03");
+                SceneManager.LoadScene("Level03");
+                break;
+        }
+    }
+
+    /// <summary>
+    /// Loads the options menu scene.
+    /// </summary>
+    public void Options()
+    {
+        PlayerPrefs.SetString("prevScene", "MainMenu");
+        SceneManager.LoadScene("Options");
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exited");
+        Application.Quit();
+    }
+}
---
+++ 0x06-unity-assets_ui/Assets/Scripts/OptionsMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class OptionsMenu : MonoBehaviour
+{
+    /// <summary>
+    /// Loads the previous scene.
+    /// </summary>
+    public void Back()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
