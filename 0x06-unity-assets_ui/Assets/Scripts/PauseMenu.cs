efa0372cddf78f8e1778860dffea0d2e9da063c7
Beardocracy <travisjbearden@gmail.com>
2020-09-23 16:14:58 -0500

    Added esc keydown for level 1

---
+++ 0x06-unity-assets_ui/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject pauseMenu;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pauseMenu.activeSelf == false)
+            {
+                this.GetComponent<Timer>().enabled = false;
+                pauseMenu.SetActive(true);
+            }
+            else
+            {
+                pauseMenu.SetActive(false);
+                this.GetComponent<Timer>().enabled = true;
+            }
+        }
+    }
+}
