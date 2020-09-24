18abd10843a04d899806e724434e7397d0d4f38f
Beardocracy <travisjbearden@gmail.com>
2020-09-23 21:28:13 -0500

    Rearranged font folder

---
+++ 0x06-unity-assets_ui/Assets/Scripts/WinMenu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class WinMenu : MonoBehaviour
+{
+    public void MainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void Next()
+    {
+        if (SceneManager.GetActiveScene().name == "Level03")
+            SceneManager.LoadScene("MainMenu");
+        if (SceneManager.GetActiveScene().name == "Level02")
+            SceneManager.LoadScene("Level03");
+        else
+            SceneManager.LoadScene("Level02");
+    }
+}
